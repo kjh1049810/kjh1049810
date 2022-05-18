9c8ec0835ad266e6eb1f799dd6412d37feab4425
kjh1049810 <dave39261049@gmail.com>
2022-05-19 08:53:55 +0900

    Calculation10

---
+++ Calculation10/Calaulation10.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO/-WARNINGS
+#include<stdio.h> 
+int main() {
+	int height;
+	printf("height = ");
+	scanf("%d", &height);
+	printf("Your height is %dcm.", height);
+	return 0;
+}
